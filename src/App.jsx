ab0009f0b5f39e078c3efbb4ccba8e0ba7c9faec
Ahmed-Wazery <158462284+Ahmed-Wazery@users.noreply.github.com>
2024-12-31 00:34:49 +0200

    component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/footer/Footer'
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
-    <h1>hamada</h1>
+    <>
+    <Navbar></Navbar>
+    <Footer></Footer>
+    </>
+
   )
 }
 
+
 export default App
+
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <nav>
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </nav>
+    </div>
+    
+  )
+}
