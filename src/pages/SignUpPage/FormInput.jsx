ef3e624d374b6babc4131b2ef642ba09a1d50423
mohamedfathy32 <mf876026@gmail.com>
2025-02-17 17:19:26 +0200

    finsh sign up user and owner in one page

---
+++ src/pages/SignUpPage/FormInput.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable react/prop-types */
+import { useState } from "react";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+
+export default function FormInput({ label, type, value, onChange, required }) {
+    const [showPassword, setShowPassword] = useState(false);
+
+    const togglePassword = () => {
+        setShowPassword((prev) => !prev);
+    };
+
+    return (
+        <div className="relative p-[1px] bg-gradient-to-r from-[#1a72ffd3] via-[#1A71FFCC] to-[#48BB78] rounded-lg">
+            <input
+                type={type === "password" && showPassword ? "text" : type}
+                value={value}
+                onChange={onChange}
+                placeholder={label}
+                required={required}
+                className="w-full p-3 bg-white rounded-lg text-right focus:outline-[#0061E0] pr-10"
+            />
+            {type === "password" && (
+                <button
+                    type="button"
+                    onClick={togglePassword}
+                    className="absolute inset-y-0 left-3 flex items-center text-[#0061E0]"
+                >
+                    {showPassword ? <AiOutlineEyeInvisible size={22} /> : <AiOutlineEye size={22} />}
+                </button>
+            )}
+        </div>
+    );
+}
