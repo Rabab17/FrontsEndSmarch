726610d9f9ead55ac5b64235b407d32c859e0655
mohamedfathy32 <mf876026@gmail.com>
2025-03-02 21:29:57 +0200

    finish renew packge

--- src/pages/OwnerDashboard/pages/SubscriptionOwner.jsx
+++ src/pages/OwnerDashboard/pages/SubscriptionOwner.jsx
@@ -2,33 +2,33 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Splash from "../../../components/Splash";
+import Swal from "sweetalert2";
 
 export default function SubscriptionOwner() {
     const [packages, setPackages] = useState([]);
     const [loading, setLoading] = useState(true);
     const token = localStorage.getItem("token");
     const navigate = useNavigate();
+    const fetchUserData = async () => {
+        try {
+            const response = await axios.get(`${import.meta.env.VITE_URL_BACKEND}subscription/user/subscriptions`,
+                {
+                    headers: {
+                        Authorization: token,
+                    },
+                }
+            );
+            setPackages(response.data.data);
+            console.log(response.data.data);
+        } catch (error) {
+            console.error("خطأ في استرجاع بيانات المستخدم:", error);
+        } finally {
+            setLoading(false);
+        }
+    };
     useEffect(() => {
-        const fetchUserData = async () => {
-            try {
-                const response = await axios.get(`${import.meta.env.VITE_URL_BACKEND}subscription/user/subscriptions`,
-                    {
-                        headers: {
-                            Authorization: token,
-                        },
-                    }
-                );
-                setPackages(response.data.data);
-                console.log(response.data.data);
-            } catch (error) {
-                console.error("خطأ في استرجاع بيانات المستخدم:", error);
-            }finally{
-                setLoading(false);
-            }
-        };
-
         fetchUserData();
-    }, [token]);
+    }, []);
 
     if (loading) {
         return <Splash />;
@@ -38,6 +38,37 @@ export default function SubscriptionOwner() {
         navigate("/OwnerDashboard/addChalet", { state: { packageId } });
     };
 
+    const handleRenewPackage = async (packageId) => {
+        setLoading(true);
+        try {
+            const response = await axios.patch(`${import.meta.env.VITE_URL_BACKEND}subscription/renew/${packageId}`, {},
+                {
+                    headers: {
+                        Authorization: token,
+                    },
+                }
+            );
+            Swal.fire({
+                title: "ناجح",
+                text: response.data.message,
+                icon: "success",
+            })
+            fetchUserData();
+            console.log(response.data.data);
+        } catch (error) {
+            Swal.fire({
+                title: "خطأ",
+                text: error.response.data.massage,
+                icon: "error",
+            });
+
+            console.error("خطأ في استرجاع بيانات المستخدم:", error);
+        } finally {
+            setLoading(false);
+        }
+    }
+
+    const currentDate = new Date();
     return (
         <div className="flex flex-col md:flex-row justify-center items-center gap-6 p-6">
             {packages.length === 0 ?
@@ -67,24 +98,33 @@ export default function SubscriptionOwner() {
                                     <h3 className="text-3xl font-bold text-blue-700 mb-4">{plan.packageId?.name}</h3>
 
                                     <div className="text-xl mb-6 space-y-2 text-start">
-                                        <p>
-                                            تاريخ الاشتراك : {new Date(plan.createdAt).toLocaleDateString("en-GB")}
-                                        </p>
-                                        <p>
-                                            تاريخ الانتهاء : {new Date(plan.endDate).toLocaleDateString("en-GB")}
-                                        </p>
-                                        <p>
-                                            عدد الشاليهات الباقيه : {plan.remainChalets}
-                                        </p>
+                                        <p>تاريخ الاشتراك : {new Date(plan.createdAt).toLocaleDateString("en-GB")}</p>
+                                        <p>تاريخ الانتهاء : {new Date(plan.endDate).toLocaleDateString("en-GB")}</p>
+                                        <p>عدد الشاليهات الباقية : {plan.remainChalets}</p>
+                                        {new Date(plan.endDate) < currentDate &&
+                                            <h1 className="text-xl text-center"> موعد الباقه انتهي برجاء التجديد</h1>
+
+                                        }
                                     </div>
-                                    <button
-                                        onClick={() => handleSelectPackage(plan._id)} // إرسال الـ id عند النقر
-                                        disabled={plan.remainChalets === 0}
-                                        className={`bg-blue-600 text-white py-2 px-8 rounded-lg hover:bg-blue-700 transition ${plan.remainChalets === 0 ? 'opacity-50 cursor-not-allowed' : ''
-                                            }`}
-                                    >
-                                        اختيار
-                                    </button>
+
+                                    {new Date(plan.endDate) < currentDate ? (
+                                        <>
+                                            <button
+                                                onClick={() => handleRenewPackage(plan._id)}
+                                                className="bg-red-600 text-white py-2 px-8 rounded-lg hover:bg-red-700 transition"
+                                            >
+                                                تجديد الاشتراك
+                                            </button>
+                                        </>
+                                    ) : (
+                                        <button
+                                            onClick={() => handleSelectPackage(plan._id)}
+                                            disabled={plan.remainChalets === 0}
+                                            className={`bg-blue-600 text-white py-2 px-8 rounded-lg hover:bg-blue-700 transition ${plan.remainChalets === 0 ? 'opacity-50 cursor-not-allowed' : ''}`}
+                                        >
+                                            اختيار
+                                        </button>
+                                    )}
                                 </div>
                             ))
                         }
