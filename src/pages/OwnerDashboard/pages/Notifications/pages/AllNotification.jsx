72693f8a2a6a4c5bd381019507373b4ccab69d89
mohamedfathy32 <mf876026@gmail.com>
2025-03-03 07:21:45 +0200

    finish project 2 push in master 2

--- src/pages/OwnerDashboard/pages/Notifications/pages/AllNotification.jsx
+++ src/pages/OwnerDashboard/pages/Notifications/pages/AllNotification.jsx
@@ -14,7 +14,7 @@ export default function AllNotification() {
         currentPage,
         toggleReadStatus, // Add this if available
     } = useContext(notificationContext);
-    
+
 
     useEffect(() => {
         getNotifications();
